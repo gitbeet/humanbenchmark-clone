3e863259bc492b68f71c24cc79a44e71b9114346
gitbeet <nikig715@gmail.com>
2023-02-20 19:29:18 +0100

    finishing up on chimp test logic

--- src/components/ChimpTest.tsx
+++ src/components/ChimpTest.tsx
@@ -5,11 +5,27 @@ const grid = new Array(40);
 const populateGrid = (numberOfBlocks: number) => {
   let arr = [...grid];
   let nums = Array.from(Array(numberOfBlocks).keys());
+  let positions = [];
+  //   account for repeating positions
   for (let i = 0; i < numberOfBlocks; i++) {
-    let position = Math.floor(Math.random() * 40);
+    let position;
+    do {
+      position = Math.floor(Math.random() * 40);
+    } while (arr[position]);
     arr.splice(position, 1, nums[i] + 1);
   }
   return arr.map((el) => ({ value: el, visible: true }));
+};
+
+const compareArr = (arr1: number[], arr2: number[]) => {
+  let res = true;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      res = false;
+      break;
+    }
+  }
+  return res;
 };
 
 const ChimpTest = () => {
@@ -19,10 +35,14 @@ const ChimpTest = () => {
   const [resultArray, setResultArray] = useState<number[]>([]);
   const [showResultScreen, setShowResultScreen] = useState(false);
   const [triesLeft, setTriesLeft] = useState(3);
+
+  let sortedArr = grid
+    .map((element) => element.value)
+    .filter((element) => element)
+    .sort((a, b) => a - b);
   useEffect(() => {
-    const isAscending = resultArray.every((element, index) => {
-      return index === 0 || element > resultArray[index - 1];
-    });
+    const isAscending = compareArr(resultArray, sortedArr);
+
     if (!isAscending) {
       setShowResultScreen(true);
       console.log("YOU LOST");
@@ -54,23 +74,67 @@ const ChimpTest = () => {
 
   return (
     <div className="w-full h-[500px] bg-blue-400 flex justify-center items-center">
-      <div className="w-fit h-fit px-16 py-12 grid grid-cols-8 grid-rows-5 gap-1">
-        {grid.map((box, index) =>
-          box.visible ? (
-            <div
-              key={index}
-              onClick={() => onItemClick(box.value, index)}
-              className={`${box.value ? "border-4 border-white" : ""} ${
-                !showNumbers && box.value && "bg-white"
-              } w-16 h-16 rounded-lg flex justify-center items-center text-white font-semibold text-3xl cursor-pointer`}
-            >
-              {showNumbers ? box.value : ""}
-            </div>
+      {showResultScreen ? (
+        <div className="text-center">
+          {triesLeft > 0 ? (
+            <>
+              <h2 className="text-xl">NUMBERS</h2>
+              <h2 className="text-xl">{blocks}</h2>
+              <br />
+              <h2>STRIKES</h2>
+              <h2>{3 - triesLeft} / 3</h2>
+              <button
+                onClick={() => {
+                  setResultArray([]);
+                  setGrid(populateGrid(blocks));
+                  setShowNumbers(true);
+                  setShowResultScreen(false);
+                }}
+              >
+                Continue
+              </button>
+            </>
           ) : (
-            <div className="w-16 h-16 rounded-lg flex justify-center items-center text-white font-semibold text-3xl cursor-pointer"></div>
-          )
-        )}
-      </div>
+            <>
+              <h2>Score</h2>
+              <h1>{blocks}</h1>
+              <button
+                onClick={() => {
+                  setBlocks(4);
+                  setGrid(populateGrid(4));
+                  setResultArray([]);
+                  setShowNumbers(true);
+                  setShowResultScreen(false);
+                  setTriesLeft(3);
+                }}
+              >
+                Try Again
+              </button>
+            </>
+          )}
+        </div>
+      ) : (
+        <div className="w-fit h-fit px-16 py-12 grid grid-cols-8 grid-rows-5 gap-1">
+          {grid.map((box, index) =>
+            box.visible ? (
+              <div
+                key={index}
+                onClick={() => onItemClick(box.value, index)}
+                className={`${box.value ? "border-4 border-white" : ""} ${
+                  !showNumbers && box.value && "bg-white"
+                } w-16 h-16 rounded-lg flex justify-center items-center text-white font-semibold text-3xl cursor-pointer`}
+              >
+                {showNumbers ? box.value : ""}
+              </div>
+            ) : (
+              <div
+                key={index}
+                className="w-16 h-16 rounded-lg flex justify-center items-center text-white font-semibold text-3xl cursor-pointer"
+              ></div>
+            )
+          )}
+        </div>
+      )}
     </div>
   );
 };
