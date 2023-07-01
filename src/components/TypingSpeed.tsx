64cb51f8a759577cda5c42b75dc2dda9002c3b6c
gitbeet <nikig715@gmail.com>
2023-07-01 17:01:05 +0200

    working on typing speed test

--- src/components/TypingSpeed.tsx
+++ src/components/TypingSpeed.tsx
@@ -1,40 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const initialText =
-  "Far from the truth, profound dollars show us how iraqs can be proses. We can assume that any instance of a scanner can be construed as a later softdrink. Those trees are nothing more than lows. A textbook of the january is assumed to be a schmalzy maid. ";
+  "Far from the truth, profound dollars show us how iraqs can be proses. We can assume that any instance of a scanner can be construed as a later softdrink. Those trees are nothing more than lows. A textbook of the january is assumed to be a schmalzy maid.";
 
 const TypingSpeed = () => {
   const [text, setText] = useState<string[]>(initialText.split(""));
   const [userInput, setUserInput] = useState<string[]>([]);
+  const [focused, setFocused] = useState(false);
+
+  const inputRef = useRef<HTMLTextAreaElement | null>(null);
   return (
-    <div>
+    <div className="game-window bg-blue flex flex-col items-center justify-center">
       <h1>Typing speed test</h1>
       <p>{userInput.length}</p>
-      <div className="relative">
-        <div>
-          <textarea
-            value={text.join("")}
-            className="absolute z-20 w-[800px] h-[200px] bg-transparent placeholder:text-black border-2"
-          />
-          <div className="absolute w-[800px] h-[200px] placeholder:text-black border-2">
+      <div className="relative flex flex-col items-center">
+        <div className="relative z-10 w-[800px] pointer-events-none ">
+          <div className="absolute z-[101] w-full  bg-[rgba(255,255,255,0)]  flex flex-wrap ">
+            {text.map((letter) => (
+              <p className={`text-neutral-900 w-fit h-fit`}>
+                {letter === " " ? "\u00A0" : letter}
+              </p>
+            ))}
+          </div>
+          <div className="absolute z-[100]     bg-[rgba(255,255,255,0)] bg-orange flex flex-wrap items-start justify-start">
             {userInput.map((letter, index) => (
-              <span
+              <p
                 className={`${
-                  letter === text[index] ? "bg-green-500" : " bg-red-400"
-                } text-transparent w-fit`}
+                  letter === text[index] ? "bg-green" : " bg-red"
+                } text-[rgba(255,255,255,0)] w-fit h-fit`}
               >
                 {letter === " " ? "\u00A0" : letter}
-              </span>
+              </p>
             ))}
           </div>
         </div>
-        <div className="relative">
-          <textarea
-            value={userInput.join("")}
-            onChange={(e) => setUserInput(e.target.value.split(""))}
-            className="absolute z-40 w-[800px] h-[200px] bg-transparent text-transparent placeholder:text-black border-2"
-          />
-        </div>
+        <textarea
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          ref={inputRef}
+          value={userInput.join("")}
+          onChange={(e) => setUserInput(e.target.value.split(""))}
+          className={`pointer-events-none caret-[rgb(255,34,34)] opacity-100 bg-[rgba(255,255,255,0)] text-[rgba(255,255,255,0)]  absolute z-[200] w-full focus:outline-none h-[200px] `}
+        />
+        <div
+          onClick={() => {
+            inputRef.current?.focus();
+            inputRef.current?.setSelectionRange(text.length, text.length);
+          }}
+          className={`${
+            focused ? "opacity-100" : "opacity-50"
+          } absolute w-[800px] h-[200px] bg-neutral-blue border border-dark-blue`}
+        ></div>
       </div>
     </div>
   );
